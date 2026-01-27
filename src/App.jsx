14c0bae2877b527a190464261beaa1e92c307d99
antwanvdm <antwanvdm@gmail.com>
2026-01-27 20:06:50 +0100

    Optimize for github pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import MazeGame from './components/maze/MazeGame';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen bg-gradient-to-b from-blue-400 to-purple-500">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/maze/:theme" element={<MazeGame />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
