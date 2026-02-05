d68021d8a1f1f7d2ad5c7896a6a429fbffba70ae
antwanvdm <antwanvdm@gmail.com>
2026-02-05 11:56:54 +0100

    Fix height mobile new try (App.jsx min-h-screen might have been conflicting.)

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import MazeGame from './components/maze/MazeGame';
 function App() {
   return (
     <HashRouter>
-      <div className="min-h-screen bg-gradient-to-b from-blue-400 to-purple-500">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/maze/:theme" element={<MazeGame />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/maze/:theme" element={<MazeGame />} />
+      </Routes>
     </HashRouter>
   );
 }
