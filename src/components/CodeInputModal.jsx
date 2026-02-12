d76b39829e44213f24678d7ec9aa6f85437a60e0
antwanvdm <antwanvdm@gmail.com>
2026-02-12 14:45:06 +0100

    Enter code styling height issue

--- src/components/CodeInputModal.jsx
+++ src/components/CodeInputModal.jsx
@@ -109,9 +109,7 @@ export default function CodeInputModal({
 
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
-      <div className={`bg-white rounded-lg max-w-2xl w-full shadow-xl flex flex-col ${
-        screen === 'enterCode' ? 'h-[90vh]' : 'max-h-[90vh]'
-      }`}>
+      <div className="bg-white rounded-lg max-w-2xl w-full shadow-xl flex flex-col max-h-[90vh]">
 
         {/* ========== WELCOME SCREEN ========== */}
         {screen === 'welcome' && (
